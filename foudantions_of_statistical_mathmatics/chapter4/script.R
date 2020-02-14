5eb4ec4ad0025645b2012c3e228050ee0f4ce02d
owari-taro <taro.biwajima@gmail.com>
2020-02-14 11:41:39 +0900

    simulation for problem5.8

--- foudantions_of_statistical_mathmatics/chapter4/script.R
+++ foudantions_of_statistical_mathmatics/chapter4/script.R
@@ -11,3 +11,44 @@ for (i in 1:n)
 
 
 hist(result)
+
+result_square=result**2
+cat("mean",mean(result_square))
+cat("var",var(result_square))
+
+
+
+#########################################
+N<-1000
+result<-c()
+
+
+for (i in 1:N){
+	
+}
+
+
+print(mean(result))
+print(sd(result))
+
+simu <- function(n,N) {
+	summary<-c()
+	for (i in 1:N){
+		x<-rnorm(n,mea=3,sd=3)
+		coef<-c(1:n)
+		total<-(2/(n*(n+1)))*sum(x*coef)
+		result<-c(result,total)
+	}	
+	paste(result)
+	return (c(mean(result),sd(result)))
+}
+
+
+
+
+
+
+
+ mydouble <- function(x) {
+   return( c(x))              # ã‚Æ“¯‚¶ŠÖ”’è‹`
+ }
